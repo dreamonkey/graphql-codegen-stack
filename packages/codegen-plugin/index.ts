75231c5d616e20d0dccad331caf45a4ad7f53c49
Yusuf Kandemir <y.kandemir@dreamonkey.com>
2023-04-09 22:23:51 +0300

    refactor(codegen-plugin): Extract query and mutation processors to separate functions

--- packages/codegen-plugin/index.ts
+++ packages/codegen-plugin/index.ts
@@ -1,5 +1,6 @@
 import { type CodegenPlugin } from '@graphql-codegen/plugin-helpers';
 import {
+  GraphQLFieldMap,
   isListType,
   isNonNullType,
   type OperationDefinitionNode,
@@ -12,7 +13,7 @@ const toPascalCase = (str: string) =>
  * Unindents a string by a given number of spaces (default 8)
  * and removes the first and last line (which are empty)
  */
-const unindent = (str: string, level = 8) =>
+const unindent = (str: string, level = 4) =>
   str
     .split('\n')
     .slice(1, -1)
@@ -34,121 +35,20 @@ const codegenPlugin: CodegenPlugin = {
     if (!queryType) {
       return '';
     }
-    const queryFields = queryType.getFields();
 
     const operations = document.definitions.filter(
       (definition): definition is OperationDefinitionNode =>
         definition.kind === 'OperationDefinition',
     );
-    const queries = operations.filter(({ operation }) => operation === 'query');
-    const mutations = operations.filter(
-      ({ operation }) => operation === 'mutation',
-    );
 
-    const queryContents = queries.map(
-      ({ name: nameNode, variableDefinitions, selectionSet }) => {
-        if (!nameNode) {
-          console.warn('[vue-apollo] Skipping unnamed query');
-          return;
-        }
-        // Queries should contain their first selection as the field name on the GraphQL schema
-        const field = selectionSet.selections[0];
-        if (field?.kind !== 'Field') {
-          console.warn('[vue-apollo] Skipping query with no field');
-          return;
-        }
-        const fieldName = field.name.value;
-        const rawName = nameNode.value;
-        const name = toPascalCase(rawName);
-        const queryName = `${name}Query`;
-        const lazyQueryName = `${name}LazyQuery`;
-        const documentName = `${name}Document`;
+    const queryFields = queryType.getFields();
+    const queryContents = operations
+      .filter(({ operation }) => operation === 'query')
+      .map(createQueryProcessor(queryFields));
 
-        const mainType = queryFields[fieldName]?.type;
-        const isNonNullableArray =
-          isNonNullType(mainType) && isListType(mainType.ofType);
-
-        const hasVariables =
-          variableDefinitions && variableDefinitions.length > 0;
-        const variablesModifier = hasVariables ? '' : ' = {}';
-
-        return unindent(`
-        export function use${queryName}(
-          variables: VueApolloQuery.VariablesParameter<${queryName}Variables>${variablesModifier},
-          options: VueApolloQuery.OptionsParameter<${queryName}, ${queryName}Variables> = {},
-        ) {
-          const useQuery = VueApollo.useQuery(${documentName}, variables, options);
-          return {
-            ...useQuery,
-            ${fieldName}: Vue.computed(() => useQuery.result.value?.${fieldName}${
-          isNonNullableArray ? ' ?? []' : ''
-        }),
-          };
-        }
-
-        export function use${lazyQueryName}(
-          variables: VueApolloQuery.VariablesParameter<${queryName}Variables>${variablesModifier},
-          options: VueApolloQuery.OptionsParameter<${queryName}, ${queryName}Variables> = {},
-        ) {
-          return VueApollo.useLazyQuery(${documentName}, variables, options);
-        }
-
-        export type ${name}CompositionFunctionResult = VueApollo.UseQueryReturn<${queryName}, ${queryName}Variables>;
-        `);
-      },
-    );
-
-    const mutationContents = mutations.map(
-      ({ name: nameNode, selectionSet }) => {
-        if (!nameNode) {
-          console.warn('[vue-apollo] Skipping unnamed mutation');
-          return;
-        }
-        // Mutations should contain their first selection as the field/mutation name on the GraphQL schema
-        const field = selectionSet.selections[0];
-        if (field?.kind !== 'Field') {
-          console.warn('[vue-apollo] Skipping mutation with no field');
-          return;
-        }
-        const fieldName = field.name.value;
-        const rawName = nameNode.value;
-        const name = toPascalCase(rawName);
-        const mutationName = `${name}Mutation`;
-        const documentName = `${name}Document`;
-
-        return unindent(`
-        export function use${mutationName}(
-          options?: VueApolloMutationOptionsParameter<${mutationName}, ${mutationName}Variables>,
-        ) {
-          const { resolveClient } = VueApollo.useApolloClient();
-          const useMutation = VueApollo.useMutation(${documentName}, options);
-          return {
-            ...useMutation,
-            ${fieldName}: async (...params: Parameters<typeof useMutation.mutate>) => {
-              const result = await useMutation.mutate(...params);
-              if (result?.errors && result.errors.length > 0) {
-                throw new ApolloCore.ApolloError({
-                  graphQLErrors: result.errors,
-                });
-              }
-              const data = result?.data?.${fieldName};
-              if (data === undefined) {${
-                /* This case will probably never happen, but we cover it just in case (unless using a custom void scalar?) */ ''
-              }
-                throw new Error('No data returned from mutation')
-              }
-
-              const { cache } = resolveClient(${
-                /* TODO: Use options.clientId here to support multiple clients */ ''
-              });
-
-              return { data, cache };
-            },
-          };
-        }
-        `);
-      },
-    );
+    const mutationContents = operations
+      .filter(({ operation }) => operation === 'mutation')
+      .map(createMutationProcessor());
 
     return {
       prepend: [
@@ -171,5 +71,119 @@ const codegenPlugin: CodegenPlugin = {
     };
   },
 };
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function createQueryProcessor(queryFields: GraphQLFieldMap<any, any>) {
+  return function processQuery({
+    name: nameNode,
+    variableDefinitions,
+    selectionSet,
+  }: OperationDefinitionNode) {
+    if (!nameNode) {
+      console.warn('[vue-apollo] Skipping unnamed query');
+      return;
+    }
+    // Queries should contain their first selection as the field name on the GraphQL schema
+    const field = selectionSet.selections[0];
+    if (field?.kind !== 'Field') {
+      console.warn('[vue-apollo] Skipping query with no field');
+      return;
+    }
+    const fieldName = field.name.value;
+    const rawName = nameNode.value;
+    const name = toPascalCase(rawName);
+    const queryName = `${name}Query`;
+    const lazyQueryName = `${name}LazyQuery`;
+    const documentName = `${name}Document`;
+
+    const mainType = queryFields[fieldName]?.type;
+    const isNonNullableArray =
+      isNonNullType(mainType) && isListType(mainType.ofType);
+
+    const hasVariables = variableDefinitions && variableDefinitions.length > 0;
+    const variablesModifier = hasVariables ? '' : ' = {}';
+
+    return unindent(`
+    export function use${queryName}(
+      variables: VueApolloQuery.VariablesParameter<${queryName}Variables>${variablesModifier},
+      options: VueApolloQuery.OptionsParameter<${queryName}, ${queryName}Variables> = {},
+    ) {
+      const useQuery = VueApollo.useQuery(${documentName}, variables, options);
+      return {
+        ...useQuery,
+        ${fieldName}: Vue.computed(() => useQuery.result.value?.${fieldName}${
+      isNonNullableArray ? ' ?? []' : ''
+    }),
+      };
+    }
+
+    export function use${lazyQueryName}(
+      variables: VueApolloQuery.VariablesParameter<${queryName}Variables>${variablesModifier},
+      options: VueApolloQuery.OptionsParameter<${queryName}, ${queryName}Variables> = {},
+    ) {
+      return VueApollo.useLazyQuery(${documentName}, variables, options);
+    }
+
+    export type ${name}CompositionFunctionResult = VueApollo.UseQueryReturn<${queryName}, ${queryName}Variables>;
+    `);
+  };
+}
+
+function createMutationProcessor() {
+  return function processMutation({
+    name: nameNode,
+    selectionSet,
+  }: OperationDefinitionNode) {
+    if (!nameNode) {
+      console.warn('[vue-apollo] Skipping unnamed mutation');
+      return;
+    }
+
+    // Mutations should contain their first selection as the field/mutation name on the GraphQL schema
+    const field = selectionSet.selections[0];
+    if (field?.kind !== 'Field') {
+      console.warn('[vue-apollo] Skipping mutation with no field');
+      return;
+    }
+
+    const fieldName = field.name.value;
+    const rawName = nameNode.value;
+    const name = toPascalCase(rawName);
+    const mutationName = `${name}Mutation`;
+    const documentName = `${name}Document`;
+
+    return unindent(`
+    export function use${mutationName}(
+      options?: VueApolloMutationOptionsParameter<${mutationName}, ${mutationName}Variables>,
+    ) {
+      const { resolveClient } = VueApollo.useApolloClient();
+      const useMutation = VueApollo.useMutation(${documentName}, options);
+      return {
+        ...useMutation,
+        ${fieldName}: async (...params: Parameters<typeof useMutation.mutate>) => {
+          const result = await useMutation.mutate(...params);
+          if (result?.errors && result.errors.length > 0) {
+            throw new ApolloCore.ApolloError({
+              graphQLErrors: result.errors,
+            });
+          }
+          const data = result?.data?.${fieldName};
+          if (data === undefined) {${
+            /* This case will probably never happen, but we cover it just in case (unless using a custom void scalar?) */ ''
+          }
+            throw new Error('No data returned from mutation')
+          }
+
+          const { cache } = resolveClient(${
+            /* TODO: Use options.clientId here to support multiple clients */ ''
+          });
+
+          return { data, cache };
+        },
+      };
+    }
+    `);
+  };
+}
 
 export default codegenPlugin;
